5c1bc436d8bec6194ceea7067fc7837a1e628b6d
Cedric Thiebault <cthiebault@p3g.org>
2014-04-02 11:38:44 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Spring Boot & Jersey
+=============
+
+Test project to evaluate [Spring Boot](http://projects.spring.io/spring-boot) and [Jersey](https://jersey.java.net).
---
+++ src/main/java/com/github/cthiebault/Application.java
@@ -0,0 +1,18 @@
+package com.github.cthiebault;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+  public static void main(String... args) throws Exception {
+    new SpringApplicationBuilder(Application.class).showBanner(false).run(args);
+  }
+
+}
+
+
+
